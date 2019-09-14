a659baec381824bc446fb4bd3bedd2e4b97ba589
Giovana Aline Bruno <giovana.aline@gmail.com>
2019-09-13 21:40:22 -0300

    primeiro commit do projeto

---
+++ src/main/java/com/gab/meucursomc/MeucursomcApplication.java
@@ -0,0 +1,13 @@
+package com.gab.meucursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeucursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeucursomcApplication.class, args);
+	}
+
+}
