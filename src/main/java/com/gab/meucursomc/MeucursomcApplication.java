cca451cace2e5c93e1013772e201ea6522b34a6c
Giovana Aline Bruno <giovana.aline@gmail.com>
2019-09-24 21:24:52 -0300

    Criando operação de instaciação

--- src/main/java/com/gab/meucursomc/MeucursomcApplication.java
+++ src/main/java/com/gab/meucursomc/MeucursomcApplication.java
@@ -1,13 +1,31 @@
 package com.gab.meucursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MeucursomcApplication {
+import com.gab.meucursomc.domain.Categoria;
+import com.gab.meucursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class MeucursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(MeucursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
